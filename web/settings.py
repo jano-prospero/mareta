ee2cc1846aa0b6c51f27bab5d3cac986c43aa41e
jano-prospero <borja.luaces@gmail.com>
2015-07-23 23:31:38 +0200

    Commit 23/07/2015 - 23:32
    + Added MySQL/MariaDB support on settings.py
    
    + Updated requirements.txt file

--- web/settings.py
+++ web/settings.py
@@ -90,11 +90,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mareta',
+        'USER': 'analyst',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT':'',
+        }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
